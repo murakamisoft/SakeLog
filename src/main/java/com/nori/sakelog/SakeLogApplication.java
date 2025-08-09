356616a257bc89976d1ff1e846cc7527fce0f54d
nori <nori@vbminigame.sakura.ne.jp>
2025-08-09 12:44:40 +0900

    初回コミット

---
+++ src/main/java/com/nori/sakelog/SakeLogApplication.java
@@ -0,0 +1,13 @@
+package com.nori.sakelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakeLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nori/sakelog/SakeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nori.sakelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
